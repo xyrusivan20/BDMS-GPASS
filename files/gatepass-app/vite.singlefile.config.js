0a61528c2451606d71375844afc6b4e763cdfc8d
xyrusivan20 <deexyrusivan@gmail.com>
2026-07-22 13:37:12 +0800

    Delete files directory

--- files/gatepass-app/vite.singlefile.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { viteSingleFile } from "vite-plugin-singlefile";
-
-export default defineConfig({
-  plugins: [react(), viteSingleFile()],
-  build: { outDir: "dist-single", assetsInlineLimit: 100000000, chunkSizeWarningLimit: 5000 },
-});
